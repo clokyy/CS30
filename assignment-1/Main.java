a5d2637ad0b64e66c9ec3d29b48f77a96ba2a095
Logan Weisgerber <64561638+clokyy@users.noreply.github.com>
2023-10-17 14:44:35 -0600

    Upload

---
+++ assignment-1/Main.java
@@ -0,0 +1,33 @@
+//                                  A4 Ascii Animal
+//                                  Logan Weisgerber
+//                                     9/11/2023
+
+
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("##############################");
+    System.out.println("#             //////          #");
+    System.out.println("#            ////             #");
+    System.out.println("#           //                #");
+    System.out.println("#  ////    /            ////  #");
+    System.out.println("#  /===                 /===  #");
+    System.out.println("#                             #");
+    System.out.println("#                             #");
+    System.out.println("#        //////////////       #");
+    System.out.println("#      ////O//////O/////      #");
+    System.out.println("#      /////////////////      #");
+    System.out.println("###############################");
+    System.out.println("        ##############");
+    System.out.println("        ##############");
+    System.out.println("      ##################");
+    System.out.println("      #####################################");
+    System.out.println("      ##################                  ");
+    System.out.println("      #####################################");
+    System.out.println("      ##################");
+    System.out.println("      #####################################");
+    System.out.println("      ##################");
+    System.out.println("      #####################################");
+  }
+}
